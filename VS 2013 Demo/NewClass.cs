8185d28b9041089955d04e6976b739959465fce8
Eric Burchell <eric.burchell@wearejust.co.uk>
2017-09-02 13:13:13 +0100

    Changes to Program.cs and added a new class

--- VS 2013 Demo/NewClass.cs
+++ VS 2013 Demo/NewClass.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace VS_2013_Demo
 {
-    class Program
+    class NewClass
     {
-        static void Main(string[] args)
-        {
-            //Changes on the Main Branch from VS 2013
-        }
     }
 }
